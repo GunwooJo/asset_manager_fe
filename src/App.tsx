cf136f7b1da7659ae10948185f3173e5b9be2791
GunwooJo <kanggi1997@naver.com>
2025-02-02 22:57:26 +0900

    feat: 대시보드 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UserJoin from './pages/user/UserJoin.tsx';
+import AuthCallback from "./pages/AuthCallback.tsx";
+import DashBoard from "./pages/DashBoard.tsx";
 
 function App() {
 
   return (
       <Router>
           <Routes>
+                <Route path="/" element={<DashBoard/>} />
               <Route path="/users/join" element={<UserJoin />} />
+              <Route path="/auth/google/callback" element={<AuthCallback />} />
           </Routes>
       </Router>
   )
---
+++ src/pages/DashBoard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SideBar from "../components/SideBar.tsx";
+
+const DashBoard: React.FC = () => {
+    return (
+        <div>
+            <SideBar/>
+        </div>
+    );
+};
+
+export default DashBoard;
